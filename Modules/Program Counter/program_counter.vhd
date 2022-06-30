c89d190545f6670131778ba18c865e897c82020d
PebPeb <67885698+PebPeb@users.noreply.github.com>
2022-06-30 17:53:01 -0500

    Create program_counter.vhd

---
+++ Modules/Program Counter/program_counter.vhd
@@ -0,0 +1,33 @@
+----------------------------------------------
+-- program_counter								--
+-- 											--
+-- Keeps track of the program location						--
+-- 								 			--
+-- Original Project : SAP-1					--
+-- By: Bryce Keen				 			--
+-- 06/30/22 								--
+----------------------------------------------
+
+
+library ieee;
+use IEEE.STD_LOGIC_1164.all;
+
+
+entity program_counter is
+	port (
+		CLK:		in STD_lOGIC;						-- Clock and LB
+	);
+end;
+
+
+architecture program_counter_arch of program_counter is	
+  
+begin
+
+	-- Logic
+
+	
+	-- Output Logic
+	
+	
+end program_counter_arch;
